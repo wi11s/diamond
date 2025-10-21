cae5f9b0c958dfedf27e12cd609710f5ae156a74
William <wms274@nyu.edu>
2025-10-21 18:35:01 -0400

    fix links text

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -4,13 +4,13 @@ export const metadata = {
 }
 
 const links: { title: string; href: string }[] = [
-  { title: 'Steppin’ out @ pianos 2', href: 'https://youtu.be/XZSUoSw4gfo' },
-  { title: 'Cash$tarr - Darling', href: 'https://www.youtube.com/watch?v=zA9VsQVUKQM' },
-  { title: 'Ka$hkenni - Cupid', href: 'https://youtu.be/MbiPFdoaEKo' },
-  { title: 'Steppin’ out @ pianos 1', href: 'https://youtu.be/UfW-7UnFlJI' },
-  { title: 'Elliot Michaels - Mitsubishi', href: 'https://youtu.be/GnSlW4nXI5A' },
-  { title: 'Rockaway short film', href: 'https://youtu.be/q---SR5yXt4' },
-  { title: 'Analog visualizer', href: 'https://youtu.be/XSrpJV9WugU' },
+  { title: 'Steppin’ out @ pianos 2 (DJ Set)', href: 'https://youtu.be/XZSUoSw4gfo' },
+  { title: 'Cash$tarr - Darling (Music Video)', href: 'https://www.youtube.com/watch?v=zA9VsQVUKQM' },
+  { title: 'Ka$hkenni - Cupid (Music Video)', href: 'https://youtu.be/MbiPFdoaEKo' },
+  { title: 'Steppin’ out @ pianos 1 (DJ Set)', href: 'https://youtu.be/UfW-7UnFlJI' },
+  { title: 'Elliot Michaels - Mitsubishi (Music Video)', href: 'https://youtu.be/GnSlW4nXI5A' },
+  { title: 'Rockaway short film (Short Film)', href: 'https://youtu.be/q---SR5yXt4' },
+  { title: 'Tape 1 (Analog Visualizer)', href: 'https://youtu.be/XSrpJV9WugU' },
 ]
 
 export default function LinksPage() {
